eba192f292a110daf9e74449c0519e05914f7f3a
loqoman <darwin.s.clark@gmail.com>
2019-07-24 18:00:57 -0700

    Start of self-written serial library.

---
+++ dep/myserialLib/serial.c
@@ -0,0 +1,28 @@
+// C library headers
+#include <stdio.h>
+#include <string.h>
+
+// Linux headers
+#include <fcntl.h>      // Contains file controls like O_RDWR
+#include <errno.h>      // Error integer and strerror() function
+#include <termios.h>    // Contains POSIX terminal control definitions
+#include <unistd.h>     // write(), read(), close()
+
+
+void main() {
+    int serial_port = open("/dev/ttyUSB0", O_RDWR);
+
+    // Check for errors
+    if (serial_port < 0) {
+        printf("Error %i from open: %s\n", errno, strerror(errno));
+    }
+
+    // Create new termios struc, we call it 'tty' for convention
+    struct termios tty;
+    memset(&tty, 0, sizeof tty);
+
+    // Read in existing settings, and handle any error
+    if(tcgetattr(serial_port, &tty) != 0) {
+        printf("Error %i from tcgetattr: %s\n", errno, strerror(errno));
+    }
+}
