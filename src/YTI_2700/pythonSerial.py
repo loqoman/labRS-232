3b536915046eca7cc87817873756fdfc02451975
loqoman <darwin.s.clark@gmail.com>
2019-08-28 12:28:10 -0700

    Few Serial Scripts

--- src/YTI_2700/pythonSerial.py
+++ src/YTI_2700/pythonSerial.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python
+# ----- Notes -----
+'''
+*.decode('ascii')
+*.encode('ascii')
+\r = carrage return
+\r\n = CRLF
+\ooo = ASCII character with octal value ooo
+'''
 import time
 import serial
 
+print("Current pySerial version: " + serial.__version__)    # Laptop is version 3.4
 # configure the serial connections (the parameters differs on the device you are connecting to)
+# some ports doo not support configurations 
 ser = serial.Serial(
-    port='/dev/ttyUSB1',
+    port='/dev/ttyUSB0',
     baudrate=9600,
-    parity=serial.PARITY_ODD,       # Relevent to device
-    stopbits=serial.STOPBITS_TWO,   # Relevent to device
-    bytesize=serial.SEVENBITS       # Relevent to device
+    parity=serial.PARITY_NONE,       # Relevent to device
+    stopbits=serial.STOPBITS_ONE,   # Relevent to device
+    bytesize=serial.EIGHTBITS       # Relevent to device
 )
 
 ser.isOpen()
@@ -32,7 +42,13 @@ while 1 :
         # let's wait one second before reading output (let's give device time to answer)
         time.sleep(1)
         while ser.inWaiting() > 0:
+            # Read size bytes from the serial port. If a timeout is set it may return less characters as requested. 
+            # With no timeout it will block until the requested number of bytes is read.
+            # Read one byte from serial
             out += ser.read(1)
+            # input = ser.read(10) - read 10 bytes
+            # input.encode/decode('ascii')
 
         if out != '':
+            out.encode('ascii')
             print ">>" + out
---
+++ src/YTI_2700/serialBlocking.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python
+import serial
+import logging
+import time
+import sys
+# Create Serial moule 
+ser = serial.Serial(
+    port='/dev/ttyUSB0',
+    baudrate=9600,
+    parity=serial.PARITY_NONE,       # Relevent to device
+    stopbits=serial.STOPBITS_ONE,   # Relevent to device
+    bytesize=serial.EIGHTBITS       # Relevent to device
+)
+
+
+logging.basicConfig(level=logging.INFO,
+                    format="%(asctime)s %(levelname)-6s: %(message)s",
+                    datefmt="%m/%d %H:%M:%S",
+                    filename="serialLogs.log",
+                    filemode="a")
+
+logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+
+logging.info("Started serialBlocking.py")
+# Main loop
+
+
+def parseRecallData(inBufSize):
+    # Add assert messsage here
+    # assert inBufSize = 67
+
+    # Read the first 5 bytes, which are the ID
+    ID = ser.read(5)
+
+    # Read the next 7 bytes, which are the measurement value
+    measurment = ser.read(7)
+
+    # Read the next 5 bytes, which are the units
+    units = ser.read(4)
+   
+    # Get them out of the input buffer
+    ser.reset_input_buffer()
+    return ID, measurment, units
+
+def parseResultReportData(inBufSize):
+    # Add assert messsage here
+    # assert inBufSize = 15
+
+    # Read the first 5 bytes, which are the ID
+    well = ser.read(3)
+
+    measurment = ser.read(5)
+    
+    ID = ser.read(4)
+
+    # Get them out of the input buffer
+    ser.reset_input_buffer()
+    return ID, measurment
+
+def parseSpew(inBufSize):
+    # Add assert messsage here
+
+    message = ser.read(inBufSize)
+
+    # Clear input buffer
+    # ser.reset_input_buffer()
+    return message
+
+while True:
+    # Sleep
+    time.sleep(.5)
+    # Clear the output string
+    out = ''
+
+    # If there is things in the output buffesr
+    if ser.inWaiting() > 0:
+        
+        logging.info("Recieved an input buffer of {}".format(ser.inWaiting()))
+
+        message = parseSpew(ser.inWaiting())
+        print(type(message))
+        '''
+        ID,measurment, units = parseRecallData(ser.inWaiting())
+        logging.info("Parsed an ID of: {}".format(ID))
+        logging.info("Parsed a measurment of: {}".format(measurment))
+        logging.info("Parsed a units of: {}".format(units))
+        '''
+        logging.info("Parsed a message of: {}".format(message.encode('hex')))
+
