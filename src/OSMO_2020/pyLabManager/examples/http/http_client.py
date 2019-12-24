2602a7e94ff77e83f10222a4ce3b8df264ee460f
loqoman <darwin.s.clark@gmail.com>
2019-12-24 10:35:44 -0800

    httpclient can be use to ping messages off of the http server for testing

--- src/OSMO_2020/pyLabManager/examples/http/http_client.py
+++ src/OSMO_2020/pyLabManager/examples/http/http_client.py
@@ -1,8 +1,10 @@
 import httplib
 
 c = httplib.HTTPConnection('localhost', 8080)
-c.request('ASSINGLABKEY', '', '{}')
+c.request('OSMO2020COMMAND', '', 'TESTSAMPLEid')
 
+response = c.getresponse()
+print(str(response.getheaders()))
 # Doing PUT
 #body_content = 'BODY CONTENT GOES HERE'
 #c.request('PUT', '', body_content)
